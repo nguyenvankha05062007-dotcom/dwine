d922ee0c3a402b44806ae6864446c58ff65b6f4a
Oroma Kaizen <standno8@gmail.com>
2026-06-30 13:57:30 +0700

    reset

--- www/pages/views/cart/success.php
+++
@@ -1,5 +0,0 @@
-<div class="text-center py-5">
-    <h2 class="mb-3">🎉 Đặt hàng thành công!</h2>
-    <p class="text-white">Mã đơn hàng của bạn là: <strong class="text-danger">#<?= $orderId ?></strong></p>
-    <a href="?page=products" class="btn btn-wine mt-3">Tiếp tục mua sắm</a>
-</div>
